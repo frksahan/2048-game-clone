1d9044bb8900ade0b2d3a66a6f39c8f260f3a010
Ömer Faruk <sahan.faruk.omer@outlook.com>
2021-12-15 23:10:27 +0300

    init project

---
+++ app.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded' ,  () => {
+    const gridDisplay = document.querySelector(' .grid ')
+    const scoreDisplay = document.getElementById(' score ')
+    const resultDisplay = document.getElementById(' results ')
+    const width = 4
+    let squares = [ ]
+
+    //create a playing board 
+    function createBoard() {
+        for (let  i=0;  i <  width*width; i++) {
+            square = document.createElement('div')
+            square.innerHTML = 0
+            gridDisplay.appendChild(square)
+            squares.push(square) 
+        }
+    }
+    createBoard()
+
+})
